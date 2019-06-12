69c59f8a96c4a2c4a4a9e698d90bedc61460faf1
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2019-06-12 12:42:04 -0400

    Adding work in progress

--- ubuntu-docker/deploy-docker-app.sh
+++ ubuntu-docker/deploy-docker-app.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-echo "Deploy the \"Hello World\" Application"
 
-# Restart the daemon
-sudo service docker restart
+# check if the docker service exists
+if service --status-all | grep -Fq 'docker'; then
+	echo "Deploy the \"Hello World\" Application"
 
-# This line builds our local application image from the Dockerfile file.
-sudo docker-compose -f /vagrant/hello_world/docker-compose.yml build
+	# Restart the daemon
+	sudo service docker restart > /dev/null 2>&1
 
-# Restart the daemon
-sudo service docker restart
+	# This line builds our local application image from the Dockerfile file.
+	sudo docker-compose -f /vagrant/hello_world/docker-compose.yml build > /dev/null 2>&1
 
-# This line runs the web and redis containers in daemon mode (-d), as specified in the docker-compose.yml file.
-sudo docker-compose -f /vagrant/hello_world/docker-compose.yml up -d
+	# Restart the daemon
+	sudo service docker restart > /dev/null 2>&1
 
-# Check that the application containers have been created by executing:
-sudo docker ps
+	# This line runs the web and redis containers in daemon mode (-d), as specified in the docker-compose.yml file.
+	sudo docker-compose -f /vagrant/hello_world/docker-compose.yml up -d > /dev/null 2>&1
 
-# Let's check to see that the application is up. We can get the IP of the helloworld_web_1 container by executing:
+	# Check that the application containers have been created by executing:
+	sudo docker ps
 
-WEB_APP_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' helloworld_web_1)
-echo $WEB_APP_IP
+	# Let's check to see that the application is up. We can get the IP of the helloworld_web_1 container by executing:
 
-# Check that the web application is returning the proper message:
-curl http://${WEB_APP_IP}:80
+	WEB_APP_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' helloworld_web_1)
+	echo "Web app available in the IP address $WEB_APP_IP"
+
+	# Check that the web application is returning the proper message:
+	WEB_APP_CONTENT=$(curl -s http://${WEB_APP_IP}:80)
+	echo "web app content: $WEB_APP_CONTENT"
+
+else
+	echo "Docker service is not available yet!"
+fi
