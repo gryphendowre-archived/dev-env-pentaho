f438e069896c5a18e0966c2d0f8b092cbd2a473a
Jean Paul Ruiz <Jean Paul Ruiz jeanpaul.ruiz@hitachivantara.com>
2019-06-15 01:40:50 -0400

    Adding changes for working ce version

--- scripts/install-gui.sh
+++ scripts/install-gui.sh
@@ -3,30 +3,33 @@
 echo "installing $2 ui for ubuntu $1"
 
 if [ "$1" == "14.04" ] ; then
-	sudo apt-get install -y ubuntu-desktop
+	sudo apt-get install -y --no-install-recommends ubuntu-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "unity" ] ; then
-	sudo apt-get install -y ubuntu-desktop > /dev/null 2>&1
+	sudo apt-get install -y --no-install-recommends ubuntu-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "kde" ] ; then
 	sudo add-apt-repository ppa:kubuntu-ppa/backports
-	sudo apt-get install -y kubuntu-desktop
+	sudo apt-get install -y --no-install-recommends kubuntu-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "lubuntu" ] ; then
-	sudo apt-get install -y lubuntu-desktop
+	sudo apt-get install -y --no-install-recommends lubuntu-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "mate" ] ; then
-	sudo apt-get install -y mate-desktop
+	sudo apt-get install -y --no-install-recommends mate-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "gnome" ] ; then
-	sudo apt-get install -y ubuntu-gnome-desktop
+	sudo apt-get install -y --no-install-recommends ubuntu-gnome-desktop
 fi
 
 if [ "$1" == "18.04" ] && [ "$2" == "xubuntu" ] ; then
-	sudo apt-get install -y xubuntu-desktop
+	sudo apt-get install -y --no-install-recommends xubuntu-desktop
 fi
+
+# todo: move this to another file
+sudo apt-get install -y --no-install-recommends firefox
